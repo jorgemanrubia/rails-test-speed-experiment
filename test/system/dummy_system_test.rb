b7498225ba054a66ea12fdd1328892b99694a552
Jorge Manrubia <jorge.manrubia@gmail.com>
2018-05-19 15:40:23 +0200

    Update readme

--- test/system/dummy_system_test.rb
+++ test/system/dummy_system_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class DummySystemTest < ApplicationSystemTestCase
   100.times do |index|
-    test "Integration test #{index}" do
+    test "System test #{index}" do
       visit test_path(label: index)
       assert_selector 'p', text: /#{index}/
     end
